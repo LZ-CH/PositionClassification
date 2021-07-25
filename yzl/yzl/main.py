3a2312f91c88d82ef0012e30b251f5ba6984fcd1
Alger <61136294+sysu19351098@users.noreply.github.com>
2021-07-25 20:14:44 +0800

    Delete yzl/yzl directory

--- yzl/yzl/main.py
+++
@@ -1,6 +0,0 @@
-from model import *
-
-# train_cost, test_cost, train_acc, test_acc = train(epoch=100, batch_size=512, fix_len=32, lr=1e-5)
-acc = evaluate(model_path='./model/best.ckpt', data_path='./data/test/')
-print('test accuracy: '+str(acc))
-
